4c9eead11718c11b0d0cb04aae70a43e163c242b
sdarrell <42746528+sdarrell@users.noreply.github.com>
2023-01-23 19:24:06 +0200

    final

---
+++ search_bar.js
@@ -0,0 +1,9 @@
+//searchBarInput = document.getElementById("SearchBar")
+//searchBarInput.addEventListener("input",updateTable)
+
+function updateTable() {
+ //filter = document.getElementById("FilterType")
+ //import data from "C:\\Users\\Shelly\\Documents\\ShellyWorkAssignment\\workers.json" assert { |: "JSON" }
+ //alert(data)
+ alert("hello world")
+ }
